618e7ba854a8ae88c56e1132df404737d34c9d89
Tien Tran <tientran2290@gmail.com>
2022-05-28 15:11:59 -0700

    Add Wallet connection provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import dynamic from 'next/dynamic'
+import '@solana/wallet-adapter-react-ui'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const WalletConnectionProvider = dynamic(
+    () => import('../context/WalletConnectionProvider'),
+    { ssr: false },
+  )
+  return <WalletConnectionProvider>
+    <Component {...pageProps} />
+  </WalletConnectionProvider>
 }
 
 export default MyApp
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
         <MainView />
       ) :
         (
-          <div class="loginContainer">
-            <div class="loginTitle">Log in to Tiktok</div>
-            <div class="loginSubtitle"> Manage your account, check notifications, comment on videos, and more.</div>
+          <div className="loginContainer">
+            <div className="loginTitle">Log in to Tiktok</div>
+            <div className="loginSubtitle"> Manage your account, check notifications, comment on videos, and more.</div>
           </div>
         )
       }
