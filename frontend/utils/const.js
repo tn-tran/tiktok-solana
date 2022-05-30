9e3f260609a5b4bea52da20419a8803c7109dc89
Tien Tran <tientran2290@gmail.com>
2022-05-30 13:44:21 -0700

    Connect frontend with solana

---
+++ frontend/utils/const.js
@@ -0,0 +1,8 @@
+import { clusterApiUrl, PublicKey } from "@solana/web3.js"
+import tiktok from './tiktok_clone.json'
+
+export const TIKTOK_PROGRAM_ID = new PublicKey(
+  "8GtYxyHhNbByJNBTTtnFjw1UPtsQMNu2jXqyMmJChv89"
+)
+
+export const TIKTOK_IDL = tiktok
