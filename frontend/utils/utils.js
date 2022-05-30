01105eadf66e3eb1d39a53327072e42e191d7404
Tien Tran <tientran2290@gmail.com>
2022-05-30 14:24:32 -0700

    Add custom hook

---
+++ frontend/utils/utils.js
@@ -0,0 +1,21 @@
+import * as anchor from '@project-serum/anchor'
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base'
+import { TIKTOK_IDL, TIKTOK_PROGRAM_ID } from './const'
+
+export function getProgramInstance(connection, wallet) {
+  if (!wallet.publicKey) throw new WalletNotConnectedError()
+
+  const provider = new anchor.AnchorProvider(
+    connection,
+    wallet,
+    anchor.AnchorProvider.defaultOptions()
+  )
+
+  const idl = TIKTOK_IDL
+
+  const programId = TIKTOK_PROGRAM_ID
+
+  const program = new (anchor).Program(idl, programId, provider)
+
+  return program
+}
