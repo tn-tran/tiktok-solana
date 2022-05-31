0fa7bfbb75b988276fd3676a77cd7c654896fd0c
Tien Tran <tientran2290@gmail.com>
2022-05-30 17:36:08 -0700

    Fix Program Id

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -4,7 +4,7 @@ import tiktok from './tiktok_clone.json'
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
 export const TIKTOK_PROGRAM_ID = new PublicKey(
-  "DpgUuHQ8SnRJG9oU3jzGJRfTcE6X36vafhcWKEX74PLB"
+  "AjugDoUKPk7WwKqh24VHhpGvwfKEgzT9XHhhXy1YgWyk"
 )
 
 export const TIKTOK_IDL = tiktok
